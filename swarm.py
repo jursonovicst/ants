26b415b573f46f1d0f49692115026264df8483b2
jursonovicst <github.com@jursonovics.com>
2019-02-28 09:51:38 +0100

    fix

--- swarm.py
+++ swarm.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
                         help='Connect to the Colony at address')
     parser.add_argument('--port', type=int, help='port to use (default: %(default)s)', default=7777)
     parser.add_argument('--nestcount', type=int, help='number of nests to create (default: number of cores -2)',
-                        default=None)
+                        default=0)
     parser.add_argument('simfile', type=argparse.FileType('r'), nargs='?')
     args = parser.parse_args()
 
@@ -20,7 +20,7 @@ if __name__ == "__main__":
     mynests = []
     if args.connect is not None:
         print("slave mode")
-        for i in range(0, (multiprocessing.cpu_count() - 2) if args.nestcount is None else args.nestcount):
+        for i in range(0, (multiprocessing.cpu_count() - 2) if args.nestcount is 0 else args.nestcount):
             mynests.append(Nest(address=args.connect[0], port=args.port,
                                 name="%s_%d" % (args.connect[1] if len(args.connect) > 1 else 'default', i)))
         exit(any(map(lambda nest: nest.exitcode, mynests)))
