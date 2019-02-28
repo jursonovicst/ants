5bb221f7c8263b67c7ea8936737eb97db447751c
jursonovicst <github.com@jursonovics.com>
2019-02-28 22:26:55 +0100

    simple example from MS

---
+++ onestream.py
@@ -0,0 +1,11 @@
+from killthebeast import *
+
+
+def execute(colony: Colony):
+    colony.addegg(Egg(5,
+                      larv=ABRAnt,
+                      name="TTL 720p VC1 CLEAR",
+                      server="playready.directtaps.net",
+                      manifestpath="/smoothstreaming/TTLSS720VC1/To_The_Limit_720.ism/Manifest",
+                      strategy=min)
+                  )
