fe5ac3f312ea8bdefe67825a5496d1ff8f599083
jursonovicst <github.com@jursonovics.com>
2019-03-01 10:36:34 +0100

    add queen to lay eggs

---
+++ killthebeast/Queen.py
@@ -0,0 +1,11 @@
+class Queen(object):
+    """
+    Create a subclass of this to lay eggs. The method layeggs() will be called by the simulation.
+    """
+
+    def layeggs(self):
+        """
+        Overload this method to yield your eggs. Must be an iterator.
+        :return:
+        """
+        pass
