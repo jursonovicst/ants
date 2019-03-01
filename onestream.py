63986646e2f222f3ec8797dcdddd09af7a20b61f
jursonovicst <github.com@jursonovics.com>
2019-03-01 10:39:04 +0100

    use the Queen class

--- onestream.py
+++ onestream.py
@@ -1,11 +1,11 @@
-from killthebeast import *
+from killthebeast import Egg, ABRAnt, Queen
 
 
-def execute(colony: Colony):
-    colony.addegg(Egg(5,
-                      larv=ABRAnt,
-                      name="TTL 720p VC1 CLEAR",
-                      server="playready.directtaps.net",
-                      manifestpath="/smoothstreaming/TTLSS720VC1/To_The_Limit_720.ism/Manifest",
-                      strategy=min)
-                  )
+class Simplequeen(Queen):
+    def layeggs(self):
+        yield Egg(5,
+                  larv=ABRAnt,
+                  name="TTL 720p VC1 CLEAR",
+                  server="playready.directtaps.net",
+                  manifestpath="/smoothstreaming/TTLSS720VC1/To_The_Limit_720.ism/Manifest",
+                  strategy=min)
