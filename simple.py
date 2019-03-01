8e7ff9ce902b4a382514b2a8c7a156f6e92358a6
jursonovicst <github.com@jursonovics.com>
2019-03-01 10:38:08 +0100

    use the Queen class

--- simple.py
+++ simple.py
@@ -1,10 +1,7 @@
-from killthebeast import *
+from killthebeast import Egg, Ant, Queen
 
 
-def execute(colony: Colony):
-    colony.addegg(Egg(0, larv=Ant, name='0'))
-    colony.addegg(Egg(1, larv=Ant, name='1'))
-    colony.addegg(Egg(2, larv=Ant, name='2'))
-    colony.addegg(Egg(3, larv=Ant, name='3'))
-    colony.addegg(Egg(4, larv=Ant, name='4'))
-    colony.addegg(Egg(5, larv=Ant, name='5'))
+class Simplequeen(Queen):
+    def layeggs(self):
+        for i in range(0, 6):
+            yield Egg(i, larv=Ant, name=str(i))
