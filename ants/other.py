ff288f8053c62a80779e712e631cf86b26484b79
jursonovicst <github.com@jursonovics.com>
2019-03-01 11:26:08 +0100

    fix Msg import error

---
+++ ants/other.py
@@ -0,0 +1,29 @@
+class Msg(object):
+
+    def __init__(self, msg: str):
+        self._msg = msg
+
+    def __str__(self):
+        return self._msg
+
+
+class Cmd(object):
+    _EXECUTE = 0
+    _TERMINATE = 1
+
+    def __init__(self, command):
+        self._command = command
+
+    @classmethod
+    def kick(cls):
+        return cls(cls._EXECUTE)
+
+    def iskick(self):
+        return self._command == self._EXECUTE
+
+    @classmethod
+    def terminate(cls):
+        return cls(cls._TERMINATE)
+
+    def isterminate(self):
+        return self._command == self._TERMINATE
