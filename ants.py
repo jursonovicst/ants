97af39b34774b9d52d101a0cad06a9b7ecdbb6c6
jursonovicst <github.com@jursonovics.com>
2019-03-19 20:52:02 +0100

    - help  - rename stuff  - add check, if profile was specified

--- ants.py
+++ ants.py
@@ -5,22 +5,21 @@ from ants import Colony, Nest, Queen
 import time
 import multiprocessing
 
-# required packages:
-# pycurl, lxml, numpy, chardet
-
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(prog='colony', description='Colony load tester')
-    parser.add_argument('--listen', type=str, metavar='address', help='Listen')
+    parser = argparse.ArgumentParser(prog='Ants', description="Ants load tester.")
+    parser.add_argument('--listen', type=str, metavar='address', help="Master mode, listens for slave connections at.")
     parser.add_argument('--connect', type=str, nargs='+', metavar=('address', 'name'),
-                        help='Connect to the Colony at address')
-    parser.add_argument('--port', type=int, help='port to use (default: %(default)s)', default=7777)
-    parser.add_argument('--nestcount', type=int, help='number of nests to create (default: number of cores -2)',
-                        default=0)
-    parser.add_argument('simfile', type=argparse.FileType('r'), nargs='?')
+                        help="Slave mode, connects to master at 'address' with 'name' identifier.")
+    parser.add_argument('--port', type=int, help='Port to use for connection (default: %(default)s)', default=7777)
+    parser.add_argument('--nestcount', type=int, help='Number of nests to create per host (default: number of cores -2)'
+                        , default=0)
+    parser.add_argument('profile', type=argparse.FileType('r'), nargs='?',
+                        help="Python file containing a subclass of the Queen class to describe the load profile.")
+
     args = parser.parse_args()
 
-    colony = None
+    mycolony = None
     mynests = []
 
     MSTANDALONE = 0
@@ -33,7 +32,6 @@ if __name__ == "__main__":
         mode = MMASTER
 
     try:
-
         if mode == MSLAVE:
             print("slave mode")
             for i in range(0, (multiprocessing.cpu_count() - 2) if args.nestcount is 0 else args.nestcount):
@@ -47,18 +45,24 @@ if __name__ == "__main__":
             exit(0)
 
         elif mode == MMASTER:
-            print("master mode")
-            colony = Colony(address=args.listen, port=args.port)
+            if 'profile' not in args:
+                raise AttributeError("Error, profile file most be defined for master mode!")
+
+            print("master mode with ")
+            mycolony = Colony(address=args.listen, port=args.port)
             time.sleep(1)
 
             # wait for nests to connect
-            input("Press Enter to load simulation...")
+            input("Connect slaves, then press enter to continue...")
 
-            # continue to load simulation
+            # continue to load
 
         else:
+            if 'profile' not in args:
+                raise AttributeError("Error, profile file most be defined for standalone mode!")
+
             print('standalone mode')
-            colony = Colony(address='127.0.0.1', port=args.port)
+            mycolony = Colony(address='127.0.0.1', port=args.port)
             time.sleep(1)
 
             for i in range(0, (multiprocessing.cpu_count() - 2) if args.nestcount is 0 else args.nestcount):
@@ -66,31 +70,31 @@ if __name__ == "__main__":
                 mynests.append(nest)
             time.sleep(1)
 
-            # continue to load simulation
+            # continue to load
 
         # load simulation Queen
-        exec(args.simfile.read())
+        exec(args.profile.read())
         if len(Queen.__subclasses__()) != 1:
-            raise SyntaxError("Simulation file '%s' must contain exactly one subclass of Queen, found: %s" % (
-                args.simfile.name, list(map(lambda cls: cls.__name__, Queen.__subclasses__()))))
+            raise SyntaxError("Profile file '%s' must contain exactly one subclass of Queen, found: %s" % (
+                args.profile.name, list(map(lambda cls: cls.__name__, Queen.__subclasses__()))))
 
         # lay the eggs
-        simqueen = Queen.__subclasses__()[0]()
-        for egg in simqueen.layeggs():
-            colony.addegg(egg)
+        myqueen = Queen.__subclasses__()[0]()
+        for egg in myqueen.layeggs():
+            mycolony.addegg(egg)
 
         # execute simulation
-        colony.execute()
+        mycolony.execute()
 
         # wait till ends
-        colony.join()
+        mycolony.join()
 
     except Exception as e:
         print(e)
-        colony.terminate()
+        mycolony.terminate()
     except KeyboardInterrupt:
         print("interrupt")
-        colony.terminate()
+        mycolony.terminate()
 
     finally:
         if mode != MMASTER:
