f5dbf752373d52fc4cfcb9d8a3770b2a17ae942c
jursonovicst <github.com@jursonovics.com>
2019-09-24 11:46:59 +0200

    add loglevels

--- ants/queen.py
+++ ants/queen.py
@@ -3,6 +3,9 @@ class Queen(object):
     Define a subclass of Queen and override its layeggs() method to lay eggs.
     """
 
+    def __init__(self, loglevel: int):
+        self._loglevel = loglevel
+
     def layeggs(self):
         """
         Override this method to yield Eggs. During initialization, it will be call as many times as it yields Eggs.
