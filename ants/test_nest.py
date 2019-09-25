d774066260382507576e63bd06d8774e6d315330
jursonovicst <github.com@jursonovics.com>
2019-09-25 12:37:52 +0200

    - register instances on class level - use signals for graceful shutdown - rewrite polling loop - remove _termiante() method

--- ants/test_nest.py
+++ ants/test_nest.py
@@ -24,40 +24,43 @@ class TestNest(TestCase):
         conn = self._listener.accept()
 
         # first message should be the created message
-        self.assertTrue(conn.poll(1), 'Nest has not sent a first (created) message within 1 second.')
+        self.assertTrue(conn.poll(0.1), 'Nest has not sent a first (started) message within 0.1s.')
         msg = conn.recv()
         self.assertIsInstance(msg, Msg)
+        self.assertRegex(str(msg), r"testnest.*started", "Thread has not started: %s" % msg)
 
+        # check ping command
         conn.send(Cmd.ping())
         msg = conn.recv()
-        self.assertIsInstance(msg, Cmd, "Thread should reply with a command, got '%s'" % type(msg))
+        self.assertIsInstance(msg, Cmd, "Thread should reply with a pong, got '%s'" % type(msg))
         self.assertTrue(msg.ispong(), "Thread should reply with a pong, got '%s'" % msg)
 
-        # terminate nest
-        conn.send(Cmd.terminate())
-        testnest.join(1)
-
-        # last message should be a terminated message
-        self.assertTrue(conn.poll(1), 'Nest has not sent the last (terminated) command within 1 second.')
-        msg = conn.recv()
-        self.assertIsInstance(msg, Cmd)
-        self.assertTrue(msg.isterminated())
+        # terminate nest, should be fast
+        testnest.terminate()
 
         # process should be dead
-        self.assertFalse(testnest.is_alive())
+        testnest.join(0.1)
+        self.assertFalse(testnest.is_alive(), "Nest should be dead after 0.1s of its termination.")
 
-        # there should not be any message
-        self.assertFalse(conn.poll(), "There should not be any message, got at least one")
+        self.assertTrue(conn.poll(0.1), 'Nest has not sent the last (terminated) message within 0.1s.')
+        msg = conn.recv()
+        self.assertIsInstance(msg, Msg)
+        self.assertRegex(str(msg), r"testnest.*terminated", "Thread has not terminated: %s" % msg)
 
+        # do not check log messages...
         conn.close()
 
     def test_ants(self):
         testnest = Nest(address='127.0.0.1', port=self._port, loglevel=Nest.DEBUG, name='testnest')
 
-        # check connection and read first message
+        # check connection
         conn = self._listener.accept()
-        self.assertTrue(conn.poll(1), 'Nest has not sent a first (created) message within 1 second.')
-        conn.recv()
+
+        # first message should be the created message
+        self.assertTrue(conn.poll(0.1), 'Nest has not sent a first (started) message within 0.1s.')
+        msg = conn.recv()
+        self.assertIsInstance(msg, Msg)
+        self.assertRegex(str(msg), r"testnest.*started", "Thread has not started: %s" % msg)
 
         # add egg with sleepyant
         sleepperiod = random.randint(2000, 10000) / 1000
@@ -72,29 +75,22 @@ class TestNest(TestCase):
         # kick nest
         conn.send(Cmd.execute())
 
-        # check ant creation messages
-        self.assertTrue(conn.poll(1.1), '__')
+        # check ant creation messages, ant should born after 1 sec
+        self.assertFalse(conn.poll(0.9), 'Ant born too early')
+        self.assertTrue(conn.poll(0.2), 'Ant has not born in 1.1s.')
         msg = conn.recv()
         self.assertRegex(str(msg), r"sleepyant.*born", "Ant has not born yet: %s" % msg)
 
-        # ant waken message must be there
-        self.assertTrue(conn.poll(sleepperiod + 0.1), "Ant's waken message hasn't received in 0.1s.")
-        msg = conn.recv()
-        self.assertRegex(str(msg), r"sleepyant.*waken", "Ant has not died yet: %s" % msg)
-
-        # ant died message must be there
-        self.assertTrue(conn.poll(0.1), "Ant's died message hasn't received in 0.1s.")
+        # ant waken message must be there, TODO: why is this so slow, why we need to wait an extra second?
+        self.assertTrue(conn.poll(sleepperiod + 1), "Ant's waken message hasn't received in %fs." % (sleepperiod + 1))
         msg = conn.recv()
         self.assertRegex(str(msg), r"sleepyant.*died", "Ant has not died yet: %s" % msg)
 
         # confirm terminate
         self.assertTrue(conn.poll(0.1), 'Command confirmation has not received in 0.1s.')
         msg = conn.recv()
-        self.assertIsInstance(msg, Cmd)
-        self.assertTrue(msg.isterminated())
-
-        # check if nest terminates
-        testnest.join(0.1)
+        self.assertIsInstance(msg, Msg)
+        self.assertRegex(str(msg), r"testnest.*terminated", "Thread has not started: %s" % msg)
 
         # process should be dead
         self.assertFalse(testnest.is_alive(), "Nest should be dead after termination.")
