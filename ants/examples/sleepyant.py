7469afff6958ca12142d0b6dec3357ebb967cc5a
jursonovicst <github.com@jursonovics.com>
2019-09-25 12:30:27 +0200

    detailed logging

--- ants/examples/sleepyant.py
+++ ants/examples/sleepyant.py
@@ -11,7 +11,7 @@ class SleepyAnt(Ant):
         if sleepperiod < 0:
             raise ValueError("sleep period must be non negative: %d" % sleepperiod)
 
-        self.schedulework(sleepperiod)
+        self.schedulework(sleepperiod, sleepperiod)
 
     def work(self, *args):
-        self._log("waken up")
+        self._log("waken up after %d seconds." % args[0])
